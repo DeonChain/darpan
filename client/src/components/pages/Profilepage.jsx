63608696c2b3f42b25283c978cbe40be31cfeb3b
Shiva0pandit <s6399587@gmail.com>
2023-12-31 14:05:04 +0530

    update

--- client/src/components/pages/Profilepage.jsx
+++ client/src/components/pages/Profilepage.jsx
@@ -1,122 +1,171 @@
 // ProfilePage.js
-import React, { useState } from 'react';
-import './css/profilepage.css';
-import Nav from "../templates/Nav"
+import React, { useState } from "react";
+import "./css/profilepage.css";
+import Nav from "../templates/Nav";
 
 const initialProfileData = {
-  name: 'John Doe',
-  email: 'john.doe@example.com',
+  name: "John Doe",
+  email: "john.doe@example.com",
   // Add more profile information as needed
 };
 
 const Profilepage = () => {
   const [profileData, setProfileData] = useState(initialProfileData);
   const [activeSection, setActiveSection] = useState("profileDetails");
+  const [activeTab, setActiveTab] = useState("first");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+
+  const handleProfileUpdate = () => {
+    // Implement profile update logic here
+    console.log("Profile updated:", { newName, newEmail });
+  };
 
   const handleSectionChange = (section) => {
     setActiveSection(section);
   };
 
-  const ResetPassword = () => {
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const handleResetPassword = () => {
-      // Implement password reset logic here
-      console.log('Password reset:', { password, confirmPassword });
-    };
-
-    return (
-      <div className={`profile-section ${activeSection === 'resetPassword' ? 'active' : ''}`}>
-        <h2>Reset Password</h2>
-        <div className="reset-password-form">
-          <label htmlFor="password">Old Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label htmlFor="confirmPassword">New Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <button onClick={handleResetPassword}>Reset Password</button>
-        </div>
-      </div>
-    );
-  };
-
-  const ProfileUpdate = () => {
-    const [newName, setNewName] = useState('');
-    const [newEmail, setNewEmail] = useState('');
-
-    const handleProfileUpdate = () => {
-      // Implement profile update logic here
-      console.log('Profile updated:', { newName, newEmail });
-    };
-
-    return (
-      <div className={`profile-section ${activeSection === 'profileUpdate' ? 'active' : ''}`}>
-        <h2>Profile Update</h2>
-        <div className="profile-update-form">
-          <label htmlFor="newName">New Name:</label>
-          <input
-            type="text"
-            id="newName"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-          <label htmlFor="newEmail">New phone:</label>
-          <input
-            type="phone"
-            id="newPhone"
-            onChange={(e) => setNewEmail(e.target.value)}
-          />
-          <label htmlFor="prodile">New profile:</label>
-          <input type="file" id="profile"name="profile" />
-          <button onClick={handleProfileUpdate}>Update Profile</button>
-        </div>
-      </div>
-    );
+  const handleResetPassword = () => {
+    // Implement password reset logic here
+    console.log("Password reset:", { password, confirmPassword });
   };
 
   return (
     <>
-    <Nav/>
-    <div className="profile-container">
-      <div className="sidebar">
-        <button onClick={() => handleSectionChange('profileDetails')}>Profile Details</button>
-        <button onClick={() => handleSectionChange('resetPassword')}>Reset Password</button>
-        <button onClick={() => handleSectionChange('profileUpdate')}>Profile Update</button>
+      <Nav></Nav>
+      <h1 className="user-head">User Profile</h1>
+      <div class="profile-container">
+        <div class="profile-image">
+          <img
+            src="https://cdn3.iconfinder.com/data/icons/web-design-and-development-2-6/512/87-1024.png"
+            alt="User Profile Image"
+          />
+        </div>
+        <div class="profile-details">
+          <h2>Joel Taylor</h2>
+          <p>
+            <b>Age:</b> 23
+          </p>
+          <p>
+            <b>Gender:</b> Male
+          </p>
+          <p>
+            <b>Email:</b> user@example.com
+          </p>
+          <p>
+            <b>Majors:</b> Btech Computer Science
+          </p>
+          <p>
+            <b>University Name:</b> GLA University Mathura
+          </p>
+          <p>
+            <b>Semester:</b>3
+          </p>
+          <p>
+            <b>Contact No:</b> 9876543211
+          </p>
+        </div>
       </div>
-      <div className="content">
-        <h1>My Profile</h1>
-        {(activeSection === 'profileDetails') && (
-          <div className="profile-section active">
-            <h2>Profile Details</h2>
-            <img src="" alt="profile image here" />
-            <div className="profile-details">
-              <p>Name: {profileData.name}</p>
-              <p>Email: {profileData.email}</p>
-              {/* Add more profile information as needed */}
-            </div>
+      <div>
+        <div className="tab-container">
+          <div
+            className={`tab ${activeTab === "first" ? "active-tab" : ""}`}
+            onClick={() => handleTabClick("first")}
+          >
+            Profile Update
           </div>
-        )}
-        {(activeSection === 'resetPassword') && <ResetPassword />}
-        {(activeSection === 'profileUpdate') && <ProfileUpdate />}
+          <div
+            className={`tab ${activeTab === "second" ? "active-tab" : ""}`}
+            onClick={() => handleTabClick("second")}
+          >
+            Reset Password
+          </div>
+          <div
+            className={`tab ${activeTab === "third" ? "active-tab" : ""}`}
+            onClick={() => handleTabClick("third")}
+          >
+            Test History
+          </div>
+        </div>
+
+        <div
+          id="first-content"
+          className={`tab-content ${
+            activeTab === "first" ? "active-content" : ""
+          }`}
+        >
+          <h2>Profile Update</h2>
+          <div className="profile-update-form">
+            <label htmlFor="newName">New Name:</label>
+            <input
+              type="text"
+              id="newName"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+            <label htmlFor="newEmail">New phone:</label>
+            <input
+              type="tel"
+              id="newPhone"
+              onChange={(e) => setNewEmail(e.target.value)}
+            />
+            <label htmlFor="prodile">New profile:</label>
+            <input type="file" id="profile" name="profile" />
+            <button onClick={handleProfileUpdate} className="profile_button">Update Profile</button>
+          </div>
+        </div>
+
+        <div
+          id="second-content"
+          className={`tab-content ${
+            activeTab === "second" ? "active-content" : ""
+          }`}
+        >
+          <h2>Reset Password</h2>
+          <div className="reset-password-form">
+            <label htmlFor="password">Old Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="confirmPassword">New Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button onClick={handleResetPassword} className="profile_button">Reset Password</button>
+          </div>
+        </div>
+
+        <div
+          id="third-content"
+          className={`tab-content ${
+            activeTab === "third" ? "active-content" : ""
+          }`}
+        >
+          <h2>Test History</h2>
+          <p>Test History Will appear hear</p>
+        </div>
       </div>
-    </div>
     </>
   );
 };
